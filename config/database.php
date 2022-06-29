6f1ea2601a98e27e2aaaf843fc10fff95e75a4b5
Rendy <auliarendy@gmail.com>
2022-06-29 14:27:24 +0700

    change db connection to hardcode

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
-<?php
 
+<?php
 use Illuminate\Support\Str;
 
 return [
@@ -48,9 +48,12 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            'database' => 'db_ijinkerja',
+            // 'username' => env('DB_USERNAME', 'forge'),
+            'username' => 'root',
+            // 'password' => env('DB_PASSWORD', ''),
+            'password' => 'Rootkb5',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
